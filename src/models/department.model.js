8b9e19eacea81399fcc2050ac4582971bbf741b6
hung purdie <nmhung.dev@gmail.com>
2022-04-20 23:06:42 +0700

    feat(model-user): define schema have made a model Users

---
+++ src/models/department.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const connectToMongoLocal = require('../configs/connectDB');
+
+const departmentSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Users',
+    },
+  ],
+  timestamps: true,
+});
+
+const Department = connectToMongoLocal.model('Departments', departmentSchema);
+
+module.exports = Department;
