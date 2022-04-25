fce9e57fd46bc77a6b1e532bfc4a5291b723e14e
hung purdie <nmhung.dev@gmail.com>
2022-04-25 23:48:12 +0700

    update category for mapping document

--- src/validations/category.validation.js
+++ src/validations/category.validation.js
@@ -1,10 +1,11 @@
-const Joi = require('joi');
 const CreateError = require('http-errors');
+const Joi = require('joi');
+Joi.ObjectId = require('joi-objectid')(Joi);
 
 const createCategory = async (req, res, next) => {
   const categorySchema = Joi.object({
     title: Joi.string().required(),
-    parentId: Joi.string().allow(null),
+    parentId: Joi.ObjectId().allow(null),
   });
   try {
     await categorySchema.validateAsync(req.body);
