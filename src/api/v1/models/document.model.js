3f1f9bfcf576353a81195dbdca6817cfb9692382
Purdie <nmhung2022@gmail.com>
2022-05-03 12:08:58 +0700

    fix: typo and missing param

--- src/api/v1/models/document.model.js
+++ src/api/v1/models/document.model.js
@@ -60,7 +60,7 @@ const documentSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    relativeDocuments: [
+    relatedDocuments: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Documents',
@@ -75,33 +75,26 @@ const documentSchema = new mongoose.Schema(
     participants: [
       {
         _id: false,
-        root: {
-          type: Boolean,
-          default: true,
-        },
-        senderId: {
+        sender: {
           ref: 'Users',
           type: mongoose.Schema.Types.ObjectId,
           required: true,
+          autopopulate: { select: 'username avatar ' },
         },
         sendDate: {
           type: Date,
           default: null,
         },
-        receivers: [
-          {
-            _id: false,
-            readDate: {
-              type: Date,
-              default: null,
-            },
-            receiverId: {
-              ref: 'Users',
-              type: mongoose.Schema.Types.ObjectId,
-              required: true,
-            },
-          },
-        ],
+        readDate: {
+          type: Date,
+          default: null,
+        },
+        receiver: {
+          ref: 'Users',
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          autopopulate: { select: 'username avatar ' },
+        },
       },
     ],
     fileList: [
