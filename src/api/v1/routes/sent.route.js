8bf2001ce06553520b6f86343d5e63228484d8df
Purdie <nmhung2022@gmail.com>
2022-05-04 15:39:08 +0700

    feat(validatie): validate objectId in param

--- src/api/v1/routes/sent.route.js
+++ src/api/v1/routes/sent.route.js
@@ -1,7 +1,25 @@
 const express = require('express');
-const router = express.Router();
 const sentController = require('../controllers/sent.controller');
+const { verifyAccessToken } = require('../middlewares/jwt');
+const verifyRoles = require('../middlewares/roles');
+const ROLES = require('../../../configs/roles.config');
+const parmaValidation = require('../validations/param.validation');
 
-router.route('/:userId').get(sentController.getSentDocuments);
+const router = express.Router();
+
+// router
+//   .route('/:userId')
+//   .get(
+//     sentController.getSentDocuments
+//   );
+
+router
+  .route('/:userId')
+  .get(
+    verifyAccessToken,
+    parmaValidation.objectId('userId'),
+    verifyRoles(ROLES.user, ROLES.admin),
+    sentController.getSentDocuments
+  );
 
 module.exports = router;
