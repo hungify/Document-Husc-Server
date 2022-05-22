1fe85513ffd2b58032ee14c7d03ac244f6816017
hungpurdie <nmhung2022@gmail.com>
2022-05-22 09:31:53 +0700

    feat(chat-message): store message to DB

--- src/api/v1/services/chat.service.js
+++ src/api/v1/services/chat.service.js
@@ -1,16 +1,6 @@
-const EVENTS = {
-  connection: 'connection',
-  CLIENT: {
-    CREATE_ROOM: 'CREATE_ROOM',
-    SEND_ROOM_MESSAGE: 'SEND_ROOM_MESSAGE',
-    JOIN_ROOM: 'JOIN_ROOM',
-  },
-  SERVER: {
-    ROOMS: 'ROOMS',
-    JOINED_ROOM: 'JOINED_ROOM',
-    ROOM_MESSAGE: 'ROOM_MESSAGE',
-  },
-};
+const Conversation = require('../models/conversation.model');
+const Message = require('../models/message.model');
+const EVENTS = require('../constants/events');
 
 const rooms = new Set();
 
@@ -18,50 +8,53 @@ function initSocket(io) {
   io.on(EVENTS.connection, (socket) => {
     socket.emit(EVENTS.SERVER.ROOMS, Array.from(rooms));
 
-    /*
-     * When a user creates a new room
-     */
     socket.on(EVENTS.CLIENT.CREATE_ROOM, ({ roomId }) => {
-      console.log({ roomId });
-      // add a new room to the rooms object
       if (!rooms.has(roomId)) {
         rooms.add(roomId);
       }
-      // rooms[roomId] = {
-      //   name: roomName,
-      // };
 
       socket.join(roomId);
 
-      // broadcast an event saying there is a new room
-      // socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
-
-      // emit back to the room creator with all the rooms
-      // socket.emit(EVENTS.SERVER.ROOMS, rooms);
-      // emit event back the room creator saying they have joined a room
       socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
     });
 
-    /*
-     * When a user sends a room message
-     */
-
     socket.on(
       EVENTS.CLIENT.SEND_ROOM_MESSAGE,
-      ({ roomId, message, username }) => {
-        const date = new Date();
-
-        socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
-          message,
-          username,
-          time: `${date.getHours()}:${date.getMinutes()}`,
+      async ({ roomId, message, username, sender }) => {
+        const foundConversation = await Conversation.findOne({
+          _id: roomId,
         });
+
+        if (foundConversation) {
+          const newMessage = new Message({
+            content: message,
+            sender,
+          });
+          await newMessage.save();
+
+          const updateConversation = await Conversation.updateOne(
+            { _id: roomId },
+            {
+              $push: {
+                messages: [newMessage._id],
+              },
+              $addToSet: {
+                members: [sender],
+              },
+            }
+          );
+          if (updateConversation.modifiedCount > 0) {
+            const date = new Date();
+            socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
+              message,
+              username,
+              time: `${date.getHours()}:${date.getMinutes()}`,
+            });
+          }
+        }
       }
     );
 
-    /*
-     * When a user joins a room
-     */
     socket.on(EVENTS.CLIENT.JOIN_ROOM, (roomId) => {
       socket.join(roomId);
 
